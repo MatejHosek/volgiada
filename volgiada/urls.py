e6a0b3e31282ba260b7c84e8ca7e3855c603a48c
Matěj Hošek <matthy.hosek@gmail.com>
2023-09-22 21:57:00 +0200

    Setup applications and their respective indexes

--- volgiada/urls.py
+++ volgiada/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
+    path("", include('view.urls')),
+    path("manage/", include('manage.urls')),
+    path("referee/", include('referee.urls')),
     path('admin/', admin.site.urls),
 ]

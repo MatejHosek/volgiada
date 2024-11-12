581b78caa1e8cefd9877d5d2442b112a06636864
Matěj Hošek <matthy.hosek@gmail.com>
2024-11-12 13:38:12 +0100

    Implement required models

--- judge/models.py
+++ judge/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Team(models.Model):
+    """A model representing competing teams"""
+
+    name = models.CharField(max_length=256)
+    school = models.CharField(max_length=256)
+
+class Problem(models.Model):
+    """A model representing competition problems"""
+
+    name = models.CharField(max_length=256, default="")
+    number = models.IntegerField()
+
+class Score(models.Model):
+    """A model representing teams' problem solutions"""
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+    points = models.IntegerField()
--- volgiada/urls.py
+++ volgiada/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("judge/", include("judge.urls")),
+    path("", include("judge.urls")),
     path('admin/', admin.site.urls),
 ]

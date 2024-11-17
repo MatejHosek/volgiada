a2b384ea4aa46bbcc4555d4b162c638ffed99b26
Matěj Hošek <matthy.hosek@gmail.com>
2024-11-17 11:11:25 +0100

    Implement point counting for Team, improve model display in Admin

--- judge/admin.py
+++ judge/admin.py
@@ -2,8 +2,23 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Team)
-admin.site.register(Problem)
-admin.site.register(Score)
-admin.site.register(Judge)
-admin.site.register(Time)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'school', 'count_points']
+
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'number']
+
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ['id', 'team', 'problem', 'points', 'time']
+
+class JudgeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'team']
+
+class TimeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'time']
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Problem, ProblemAdmin)
+admin.site.register(Score, ScoreAdmin)
+admin.site.register(Judge, JudgeAdmin)
+admin.site.register(Time, TimeAdmin)
--- judge/models.py
+++ judge/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Team(models.Model):
     """A model representing competing teams"""
@@ -10,6 +11,13 @@ class Team(models.Model):
     def __str__(self):
         return f'{self.name} ({self.school})'
 
+    def count_points(self, time = timezone.now()):
+        points = 0
+        for score in self.score_set.filter(time__lte=time):
+            points += score.points
+
+        return points
+
 class Problem(models.Model):
     """A model representing competition problems"""
 
@@ -17,7 +25,7 @@ class Problem(models.Model):
     number = models.IntegerField()
 
     def __str__(self):
-        return f'Problem {self.number}: {self.name}'
+        return f'{self.number}: {self.name}'
 
 class Score(models.Model):
     """A model representing teams' problem solutions"""
@@ -26,6 +34,9 @@ class Score(models.Model):
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
     points = models.IntegerField()
     time = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.team}-{self.problem}: {self.points} at {self.time}'
 
 class Judge(models.Model):
     """A model representing teams assigned to a judge"""

29abf03297ce5a9fb2eea698ae344d31f87daab9
Matěj Hošek <matthy.hosek@gmail.com>
2024-12-03 19:49:44 +0100

    Implement permission checking

--- judge/views.py
+++ judge/views.py
@@ -3,6 +3,14 @@ from django.http import HttpResponse
 
 from .models import *
 
+def check_permissions(request, group):
+    """Check if user is in a given group"""
+
+    # Check if user is in the group
+    if request.user.groups.filter(name=group).exists():
+        return True
+    return False
+
 def index(request):
     # Check if the competition has begun
     competitionStart = Time.objects.get(name='start').time
@@ -31,6 +39,14 @@ def index(request):
     return render(request, 'judge/index.html', context)
 
 def judge_view(request):
+    # Check for user auth
+    if request.user.is_anonymous:
+        # Prompt the user to log in
+        return login_view(request)
+
+    if not check_permissions(request, 'competition_judge'):
+        return HttpResponse('403 Forbidden', status=403)
+    
     # TODO: Display assigned teams
     return HttpResponse(request.user)
 
@@ -39,9 +55,18 @@ def team_view(request):
     pass
 
 def admin_view(request):
+    # Check for user auth
+    if request.user.is_anonymous:
+        # Prompt the user to log in
+        return login_view(request)
+
+    if not check_permissions(request, 'competition_admin'):
+        return HttpResponse('403 Forbidden', status=403)
+    
     # TODO: Display teams, problems, judges, competition settings
     return HttpResponse('admin view')
 
 def login_view(request):
     # TODO: A login page, redirects back to a page after a successful login
-    pass
+    # TODO: Implement a login page
+    return HttpResponse('Please log in')
